0d9187084dfcf752eb12e695edbe9a7fdb6763d0
Your Name <you@example.com>
2024-06-06 00:01:57 -0700

    Work In Progress

--- src/Person.java
+++ src/Person.java
@@ -6,10 +6,11 @@ class Player implements Comparable<Player> {
 
     // Body measurements
     double height; // In inches
-    int weight; // in lbs
+    double weight; // in lbs
     int wingspan; // In inches
     double handLength; // In Inches
     double handWidth; // In inches
+    double standingReach; // In inches
 
     String team;
 
@@ -18,17 +19,27 @@ class Player implements Comparable<Player> {
     double reboundsPerGame;
     double assistsPerGame;
     double stealsPerGame;
+    double blocksPerGame;
     double fieldGoalPercentage;
     double threePointPercentage;
     double freeThrowPercentage;
 
-    Player(String name, String position, int height, int weight, int wingspan, double handLength, String team,
-           double pointsPerGame, double reboundsPerGame, double assistsPerGame, double stealsPerGame,
+    Player(String name, String position, int height, double weight, int wingspan, double handLength, double handWidth, double standingReach,
+           double pointsPerGame, double reboundsPerGame, double assistsPerGame, double stealsPerGame, double blocksPerGame,
            double fieldGoalPercentage, double threePointPercentage, double freeThrowPercentage) {
+        this.name = name;
+        this.position = position;
+        this.height = height;
+        this.weight = weight;
+        this.wingspan = wingspan;
+        this.handLength = handLength;
+        this.handWidth = handWidth;
+        this.standingReach = standingReach;
         this.pointsPerGame = pointsPerGame;
         this.reboundsPerGame = reboundsPerGame;
         this.assistsPerGame = assistsPerGame;
         this.stealsPerGame = stealsPerGame;
+        this.blocksPerGame = blocksPerGame;
         this.fieldGoalPercentage = fieldGoalPercentage;
         this.threePointPercentage = threePointPercentage;
         this.freeThrowPercentage = freeThrowPercentage;
@@ -40,20 +51,34 @@ class Player implements Comparable<Player> {
 
     public double calculateScore() {
         double score = 0;
-        score += this.pointsPerGame * 0.4;
-        score += this.reboundsPerGame * 0.15;
-        score += this.assistsPerGame * .10;
-        score += this.stealsPerGame * .10;
-        score += this.fieldGoalPercentage * .20;
-        score += this.threePointPercentage * .15;
-        score += this.freeThrowPercentage * .05;
-        score += this.height * .10;
-        score += this.weight * .04;
-        score += this.wingspan * .2;
-        score += this.handLength * .02;
-        score += this.handWidth * .02;
+        score += this.pointsPerGame * 0.261;
+        score += this.reboundsPerGame * 0.098;
+        score += this.assistsPerGame * .065;
+        score += this.stealsPerGame * .065;
+        score += this.fieldGoalPercentage * .131;
+        score += this.threePointPercentage * .098;
+        score += this.freeThrowPercentage * .033;
+        score += this.height * .065;
+        score += this.weight * .026;
+        score += this.wingspan * .131;
+        score += this.handLength * .013;
+        score += this.handWidth * .013;
         return score;
     }
 
     // toString method to print player details
+    @Override
+    public String toString() {
+        return name + " (" + position + ")" + calculateScore();
+    }
+
+    // Main
+    public static void main(String[] args) {
+        Player wemby = new Player("Victor Wembanyama", "C", 89, 210,96,11,12, 116,
+                21.4, 10.6, 3.9, 1.2,3.6,  46.5, 32.5, 79.6);
+
+        Player rob = new Player("Rob Dillingham", "PG", 73, 164.2,75, 8, 8.25,95,
+                15.2, 2.9, 3.9, 1.0, .1, 47.5, 44.4, 79.6);
+
+    }
 }
