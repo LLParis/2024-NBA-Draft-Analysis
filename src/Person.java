cc46fc051dff30cab61d51f3b8213ed73396a42f
Your Name <you@example.com>
2024-06-06 10:15:22 -0700

    Work In Progress

--- src/Person.java
+++ src/Person.java
@@ -7,7 +7,7 @@ class Player implements Comparable<Player> {
     // Body measurements
     double height; // In inches
     double weight; // in lbs
-    int wingspan; // In inches
+    double wingspan; // In inches
     double handLength; // In Inches
     double handWidth; // In inches
     double standingReach; // In inches
@@ -24,7 +24,7 @@ class Player implements Comparable<Player> {
     double threePointPercentage;
     double freeThrowPercentage;
 
-    Player(String name, String position, int height, double weight, int wingspan, double handLength, double handWidth, double standingReach,
+    Player(String name, String position, double height, double weight, double wingspan, double handLength, double handWidth, double standingReach,
            double pointsPerGame, double reboundsPerGame, double assistsPerGame, double stealsPerGame, double blocksPerGame,
            double fieldGoalPercentage, double threePointPercentage, double freeThrowPercentage) {
         this.name = name;
@@ -69,8 +69,13 @@ class Player implements Comparable<Player> {
     // toString method to print player details
     @Override
     public String toString() {
-        return name + " (" + position + ")" + calculateScore();
+        return name + " (" + position + ") " + " Draft Score: " + calculateScore();
     }
+
+    public String name() {
+        return this.name;
+    }
+
 
     // Main
     public static void main(String[] args) {
@@ -80,5 +85,32 @@ class Player implements Comparable<Player> {
         Player rob = new Player("Rob Dillingham", "PG", 73, 164.2,75, 8, 8.25,95,
                 15.2, 2.9, 3.9, 1.0, .1, 47.5, 44.4, 79.6);
 
+        Player alex = new Player("Alexandre Sarr", "C", 83.75, 224.2, 88.25, 9, 10, 110,
+                9.7, 4.4, 0.9, .5, 1.3, 52.0, 29.8, 71.4);
+
+        Player zach = new Player("Zach Edey", "C", 87.75, 299, 94.75, 10, 10.75, 115,
+                25.2, 12.2, 2,.3, 2.2, 62.3, 50.0, 71.1);
+
+        ArrayList<Player> players = new ArrayList<>();
+        players.add(wemby);
+        players.add(rob);
+        players.add(alex);
+        players.add(zach);
+
+        // Display list of players
+        for (Player prospect : players) {
+            System.out.println(prospect.toString());
+        }
+
+        // Player comparison
+        System.out.println(comparePlayers(alex, zach));
+    }
+
+    public static String comparePlayers(Player p1, Player p2) {
+        if (p1.calculateScore() > p2.calculateScore()) {
+            return p1.name() + " has a higher draft score than " + p2.name;
+        } else {
+            return p2.name + " has a higher draft score than " + p1.name;
+        }
     }
 }
